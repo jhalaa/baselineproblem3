9786236cf68ef0c4b8358b28b0b0d7c6c8caeca5
jhalaa <jhalaachinoy@gmail.com>
2015-09-10 11:51:56 +0530

    refactored code to make input string state of the class

--- src/com/thoughtworks/baseline/SalesTax.java
+++ src/com/thoughtworks/baseline/SalesTax.java
@@ -5,23 +5,29 @@ import static java.lang.Double.parseDouble;
 //calculates the total tax of item
 public class SalesTax {
 
-    public double basicSalesTaxPercentage(String inputString) {
+    String inputString;
+
+    public SalesTax(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public double basicSalesTaxPercentage() {
         if(inputString.contains("chocolates") || inputString.contains("chocolate") || inputString.contains("book") || inputString.contains("pill"))
             return 0.0;
         return 10.0;
     }
 
-    public double importTaxPercentage(String inputString) {
+    public double importTaxPercentage() {
         if(inputString.contains("imported"))
             return 5.0;
         return 0.0;
     }
 
-    public double totalTax(String inputString) {
+    public double totalTax() {
         String[] inputArray = inputString.split(" ");
         double total = parseDouble(inputArray[inputArray.length - 1]);
-        double importTax = total * importTaxPercentage(inputString)/100;
-        double salesTax =total * basicSalesTaxPercentage(inputString)/100;
+        double importTax = total * importTaxPercentage()/100;
+        double salesTax =total * basicSalesTaxPercentage()/100;
 
         return  total+ salesTax +importTax;
     }
--- test/com/thoughtworks/baseline/SalesTaxTest.java
+++ test/com/thoughtworks/baseline/SalesTaxTest.java
@@ -8,63 +8,63 @@ public class SalesTaxTest {
 
     @Test
     public void shouldReturnBasicSalesTaxAsTenPercentForAllGoods() {
-        SalesTax salesTax = new SalesTax();
+        SalesTax salesTax = new SalesTax("xyz");
 
-        assertEquals(salesTax.basicSalesTaxPercentage("xyz"), 10.0);
+        assertEquals(salesTax.basicSalesTaxPercentage(), 10.0);
     }
 
     @Test
     public void shouldReturnBasicSalesTaxAsZeroPercentForChocolates() {
-        SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.basicSalesTaxPercentage("chocolates"),0.0);
+        SalesTax salesTax = new SalesTax("chocolates");
+        assertEquals(salesTax.basicSalesTaxPercentage(),0.0);
     }
 
     @Test
     public void shouldReturnBasicSalesTaxAsZeroPercentForChocolate() {
-        SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.basicSalesTaxPercentage("chocolate"),0.0);
+        SalesTax salesTax = new SalesTax("chocolate");
+        assertEquals(salesTax.basicSalesTaxPercentage(),0.0);
     }
 
     @Test
     public void shouldReturnBasicSalesTaxAsZeroPercentForBook() {
-        SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.basicSalesTaxPercentage("book"),0.0);
+        SalesTax salesTax = new SalesTax("book");
+        assertEquals(salesTax.basicSalesTaxPercentage(),0.0);
     }
 
     @Test
     public void shouldReturnBasicSalesTaxAsZeroPercentForPill() {
-        SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.basicSalesTaxPercentage("pill"),0.0);
+        SalesTax salesTax = new SalesTax("pill");
+        assertEquals(salesTax.basicSalesTaxPercentage(),0.0);
     }
 
     @Test
     public void shouldReturnImportTaxAsZeroPercentForAllItems() {
-        SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.importTaxPercentage("pill"),0.0);
+        SalesTax salesTax = new SalesTax("pill");
+        assertEquals(salesTax.importTaxPercentage(),0.0);
     }
 
     @Test
     public void shouldReturnImportTaxAsFivePercentForImportedItems() {
-        SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.importTaxPercentage("imported pill"),5.0);
+        SalesTax salesTax = new SalesTax("imported pill");
+        assertEquals(salesTax.importTaxPercentage(),5.0);
     }
 
     @Test
     public void shouldReturnTotalTaxedPriceOfAnItemWhichHasNoSalesTaxAndIsNotImported() {
-        SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.totalTax("pill at 3.00"),3.0);
+        SalesTax salesTax = new SalesTax("pill at 3.00");
+        assertEquals(salesTax.totalTax(),3.0);
     }
 
     @Test
     public void shouldReturnTotalTaxedPriceOfAnItemWhichHasNoSalesTaxAndIsImported() {
-        SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.totalTax("imported pill at 10.00"),10.50);
+        SalesTax salesTax = new SalesTax("imported pill at 10.00");
+        assertEquals(salesTax.totalTax(),10.50);
     }
 
     @Test
     public void shouldReturnTotalTaxedPriceOfAnItemWhichSalesTaxAndIsImported() {
-        SalesTax salesTax = new SalesTax();
-        assertEquals(salesTax.totalTax("imported perfume at 47.50"),54.625);
+        SalesTax salesTax = new SalesTax("imported perfume at 47.50");
+        assertEquals(salesTax.totalTax(),54.625);
     }
 
 }
