f4ca63c902bc6c5be4e708bff0439161094e6bbd
jhalaa <jhalaachinoy@gmail.com>
2015-09-10 12:29:45 +0530

    changed the state of class to arraylist of srings rather than array of strings and refactored

--- src/com/thoughtworks/baseline/SalesTax.java
+++ src/com/thoughtworks/baseline/SalesTax.java
@@ -1,33 +1,44 @@
 package com.thoughtworks.baseline;
 
+import java.util.ArrayList;
+
 import static java.lang.Double.parseDouble;
 
 //calculates the total tax of item
 public class SalesTax {
 
-    String inputString;
+    private ArrayList<String> inputString;
 
-    public SalesTax(String inputString) {
+    public SalesTax(ArrayList<String> inputString) {
         this.inputString = inputString;
     }
+    public double performOperation() {
+        int i =0;
+        double tax =0;
+        while(!(inputString.isEmpty())) {
+            tax += totalTax(inputString.get(i));
+            inputString.remove(i);
+        }
+        return tax;
+    }
 
-    public double basicSalesTaxPercentage() {
+    public double basicSalesTaxPercentage(String inputString) {
         if(inputString.contains("chocolates") || inputString.contains("chocolate") || inputString.contains("book") || inputString.contains("pill"))
             return 0.0;
         return 10.0;
     }
 
-    public double importTaxPercentage() {
-        if(inputString.contains("imported"))
+    public double importTaxPercentage(String input) {
+        if(input.contains("imported"))
             return 5.0;
         return 0.0;
     }
 
-    public double totalTax() {
-        String[] inputArray = inputString.split(" ");
+    public double totalTax(String input) {
+        String[] inputArray = input.split(" ");
         double total = parseDouble(inputArray[inputArray.length - 1]);
-        double importTax = total * importTaxPercentage()/100;
-        double salesTax =total * basicSalesTaxPercentage()/100;
+        double importTax = total * importTaxPercentage(input)/100;
+        double salesTax =total * basicSalesTaxPercentage(input)/100;
 
         return  total+ salesTax +importTax;
     }
--- test/com/thoughtworks/baseline/SalesTaxTest.java
+++ test/com/thoughtworks/baseline/SalesTaxTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.baseline;
 
 import org.junit.Test;
+import java.util.ArrayList;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -8,63 +9,100 @@ public class SalesTaxTest {
 
     @Test
     public void shouldReturnBasicSalesTaxAsTenPercentForAllGoods() {
-        SalesTax salesTax = new SalesTax("xyz");
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("xyz");
+        SalesTax salesTax = new SalesTax(inputString);
 
-        assertEquals(salesTax.basicSalesTaxPercentage(), 10.0);
+        assertEquals(salesTax.basicSalesTaxPercentage("xyz"), 10.0);
     }
 
     @Test
     public void shouldReturnBasicSalesTaxAsZeroPercentForChocolates() {
-        SalesTax salesTax = new SalesTax("chocolates");
-        assertEquals(salesTax.basicSalesTaxPercentage(),0.0);
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("chocolates");
+        SalesTax salesTax = new SalesTax(inputString);
+
+        assertEquals(salesTax.basicSalesTaxPercentage("chocolates"),0.0);
     }
 
     @Test
     public void shouldReturnBasicSalesTaxAsZeroPercentForChocolate() {
-        SalesTax salesTax = new SalesTax("chocolate");
-        assertEquals(salesTax.basicSalesTaxPercentage(),0.0);
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("chocolate");
+        SalesTax salesTax = new SalesTax(inputString);
+
+        assertEquals(salesTax.basicSalesTaxPercentage("chocolate"),0.0);
     }
 
     @Test
     public void shouldReturnBasicSalesTaxAsZeroPercentForBook() {
-        SalesTax salesTax = new SalesTax("book");
-        assertEquals(salesTax.basicSalesTaxPercentage(),0.0);
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("book");
+        SalesTax salesTax = new SalesTax(inputString);
+
+        assertEquals(salesTax.basicSalesTaxPercentage("book"),0.0);
     }
 
     @Test
     public void shouldReturnBasicSalesTaxAsZeroPercentForPill() {
-        SalesTax salesTax = new SalesTax("pill");
-        assertEquals(salesTax.basicSalesTaxPercentage(),0.0);
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("pill");
+        SalesTax salesTax = new SalesTax(inputString);
+
+        assertEquals(salesTax.basicSalesTaxPercentage("pill"),0.0);
     }
 
     @Test
     public void shouldReturnImportTaxAsZeroPercentForAllItems() {
-        SalesTax salesTax = new SalesTax("pill");
-        assertEquals(salesTax.importTaxPercentage(),0.0);
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("chocolates");
+        SalesTax salesTax = new SalesTax(inputString);
+
+        assertEquals(salesTax.importTaxPercentage("chocolates"),0.0);
     }
 
     @Test
     public void shouldReturnImportTaxAsFivePercentForImportedItems() {
-        SalesTax salesTax = new SalesTax("imported pill");
-        assertEquals(salesTax.importTaxPercentage(),5.0);
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("imported pills");
+        SalesTax salesTax = new SalesTax(inputString);
+
+        assertEquals(salesTax.importTaxPercentage("imported pills"),5.0);
     }
 
     @Test
     public void shouldReturnTotalTaxedPriceOfAnItemWhichHasNoSalesTaxAndIsNotImported() {
-        SalesTax salesTax = new SalesTax("pill at 3.00");
-        assertEquals(salesTax.totalTax(),3.0);
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("pill at 3.00");
+        SalesTax salesTax = new SalesTax(inputString);
+        assertEquals(salesTax.totalTax("pill at 3.00"),3.0);
     }
 
     @Test
     public void shouldReturnTotalTaxedPriceOfAnItemWhichHasNoSalesTaxAndIsImported() {
-        SalesTax salesTax = new SalesTax("imported pill at 10.00");
-        assertEquals(salesTax.totalTax(),10.50);
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("imported pill at 10.00");
+        SalesTax salesTax = new SalesTax(inputString);
+        assertEquals(salesTax.totalTax("imported pill at 10.00"),10.50);
     }
 
     @Test
     public void shouldReturnTotalTaxedPriceOfAnItemWhichSalesTaxAndIsImported() {
-        SalesTax salesTax = new SalesTax("imported perfume at 47.50");
-        assertEquals(salesTax.totalTax(),54.625);
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("imported perfume at 47.50");
+        SalesTax salesTax = new SalesTax(inputString);
+        assertEquals(salesTax.totalTax("imported perfume at 47.50"),54.625);
+    }
+
+    @Test
+    public void shouldCalculateTheTotalTaxOfAListOfItems() {
+        ArrayList<String> inputString = new ArrayList<String>();
+        inputString.add("book at 12.49");
+        inputString.add("CD at 14.99");
+        inputString.add("chocolate at 0.85");
+
+        SalesTax salesTax = new SalesTax(inputString);
+        assertEquals(salesTax.performOperation(),29.829);
     }
 
 }
